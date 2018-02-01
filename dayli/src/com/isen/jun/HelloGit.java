643607be4e1812979d852f6f2f073363d7df4e41
jun <jun@192.168.10.124>
2018-02-01 14:12:20 +0800

    Init isen jun git

---
+++ dayli/src/com/isen/jun/HelloGit.java
@@ -0,0 +1,13 @@
+package com.isen.jun;  
+/**   
+* 类说明  
+* @author Kevin  
+* @date 2018年2月1日 下午2:09:10 
+* @version 2.0 
+*/
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git! Init!");
+	}
+}
+ 
