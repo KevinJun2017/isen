e1da32b7ebf69a691f3197ef77e7dc23eb44028d
jun <jun@192.168.10.124>
2018-02-01 14:21:19 +0800

    second file

---
+++ dayli/src/com/isen/jun/SecondCI.java
@@ -0,0 +1,13 @@
+package com.isen.jun;  
+/**   
+* 类说明  
+* @author Kevin  
+* @date 2018年2月1日 下午2:17:40 
+* @version 2.0 
+*/
+public class SecondCI {
+	public static void main(String[] args) {
+		System.out.println("Second Isen jun Git!");
+	}
+}
+ 
